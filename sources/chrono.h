b4c62065d3cc9d2b227d1858ba760e4482e1a812
Algiane <algiane.froehly@inria.fr>
2019-02-26 15:09:11 +0100

    windows compatibility

--- sources/chrono.h
+++ sources/chrono.h
@@ -2,8 +2,21 @@
 #define _CHRONO_H
 
 #include <time.h>
+
+#if ( defined(_WIN16) || defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__TOS_WIN__) || defined(__WINDOWS__) )
+
+#else
+
+#define POSIX
+
+#endif
+
+#ifndef POSIX
+#include <windows.h>
+#else
 #include <sys/time.h>
 #include <sys/resource.h>
+#endif
 
 #ifndef  ON
 #define  RESET  0
@@ -17,8 +30,16 @@
 
 typedef struct mytime {
   double  gini,gend,gdif,uini,uend,udif,sini,send,sdif;
+#ifdef POSIX
   struct  timeval rutim;
   struct  rusage  ru;
+#else
+  HANDLE        thisProcess;
+  FILETIME      ftIni, ftEnd, ftSys, ftUser;
+  SYSTEMTIME    stSys, stUser;
+  LARGE_INTEGER frequency;
+  LARGE_INTEGER rutim;
+#endif
   int     call;
 } mytime;
 
